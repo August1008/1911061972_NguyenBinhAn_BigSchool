af780c169cc3cfa852cab13ac6d4197bf6c504e1
ngbin <ngbin@DESKTOP-GB9K06E>
2021-07-02 16:10:10 +0700

    test

--- Models/Course.cs
+++ Models/Course.cs
@@ -19,7 +19,7 @@ namespace _1911061972_NguyenBinhAn_BigSchool.Models
         public DateTime datetime { set; get; }
         public Category Category { set; get; }
         [ForeignKey("Category")]
-        public int CategoryId { set; get; }
+        public int CategoryId { set; get; } // hehe
 
     }
 }
